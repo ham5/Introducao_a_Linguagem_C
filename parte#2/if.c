b37ca9c95839b179b79e4c6be7503f594cfdd9fb
Heitor Assis <ham5@cin.ufpe.br>
2023-02-20 19:08:15 -0300

    first commit

--- parte#2/if.c
+++ parte#2/if.c
@@ -8,29 +8,28 @@ int main(){
     scanf("%d %d", &num_1, &num_2);
 
     // se esta condição for verdadeira, a instrução que está dentro será executada
-    if (num_1 == num_2){
+    if (num_1 == num_2)
         printf("%d é igual a %d\n", num_1, num_2);
-    }
+    
     // se esta condição for verdadeira, a instrução que está dentro será executada
-    if (num_1 != num_2){
+    if (num_1 != num_2)
         printf("%d é diferente de %d\n", num_1, num_2);
-    }
+    
     // se esta condição for verdadeira, a instrução que está dentro será executada
-    if (num_1 > num_2){
+    if (num_1 > num_2)
         printf("%d é maior que %d\n", num_1, num_2);
-    }
+    
     // se esta condição for verdadeira, a instrução que está dentro será executada
-    if (num_1 >= num_2){
+    if (num_1 >= num_2)
         printf("%d é maior ou igual a %d\n", num_1, num_2);
-    }
+    
     // se esta condição for verdadeira, a instrução que está dentro será executada
-    if (num_1 < num_2){
+    if (num_1 < num_2)
         printf("%d é menor que %d\n", num_1, num_2);
-    }
+    
     // se esta condição for verdadeira, a instrução que está dentro será executada
-    if (num_1 <= num_2){
+    if (num_1 <= num_2)
         printf("%d é menor ou igual a %d\n", num_1, num_2);
-    }
 
     return 0;    
 }
