615b425e9bce2ab769cc716c9d7f8f97edcd3e6b
Heitor Assis <ham5@cin.ufpe.br>
2023-02-20 23:03:57 -0300

    Update if.c

--- parte#2/if.c
+++ parte#2/if.c
@@ -1,4 +1,4 @@
-// programa que informa a relação entre dois inteiros
+/* relação entre dois inteiros */
 #include <stdio.h>
 
 int main(){
@@ -11,27 +11,21 @@ int main(){
     printf("Informe o valor do segundo inteiro:\n");
     scanf("%d", &num_2);
 
-    // se esta condição for verdadeira, a instrução que está dentro será executada
     if (num_1 == num_2)
         printf("%d é igual a %d\n", num_1, num_2);
     
-    // se esta condição for verdadeira, a instrução que está dentro será executada
     if (num_1 != num_2)
         printf("%d é diferente de %d\n", num_1, num_2);
     
-    // se esta condição for verdadeira, a instrução que está dentro será executada
     if (num_1 > num_2)
         printf("%d é maior que %d\n", num_1, num_2);
     
-    // se esta condição for verdadeira, a instrução que está dentro será executada
     if (num_1 >= num_2)
         printf("%d é maior ou igual a %d\n", num_1, num_2);
     
-    // se esta condição for verdadeira, a instrução que está dentro será executada
     if (num_1 < num_2)
         printf("%d é menor que %d\n", num_1, num_2);
     
-    // se esta condição for verdadeira, a instrução que está dentro será executada
     if (num_1 <= num_2)
         printf("%d é menor ou igual a %d\n", num_1, num_2);
 
