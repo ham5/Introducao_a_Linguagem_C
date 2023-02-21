aa5f7d56834ecd3ac2eec4479e2c00a5e2172aac
Heitor Assis <ham5@cin.ufpe.br>
2023-02-20 23:01:04 -0300

    Update soma_inteiros.c

--- parte#1/soma_inteiros.c
+++ parte#1/soma_inteiros.c
@@ -1,22 +1,19 @@
-// programa que soma dois números inteiros
+/*  somando dois números inteiros */
 #include <stdio.h>
 
 int main(){
-    int num_1;  // variável que armazenará o primeiro inteiro 
-    int num_2;  // variável que armazenará o segundo inteiro
-    int soma;   // variável que armazenará a soma
+    int num_1;
+    int num_2;  
+    int soma;
     
-    // solicita o valor do primeiro inteiro e armazena em num_1
     printf("Digite o valor do primeiro inteiro:\n");
     scanf("%d", &num_1);
-    // solicita o valor do segundo inteiro e armazena em num_2
+    
     printf("Digite o valor do segundo inteiro:\n");
     scanf("%d", &num_2);
     
-    // realiza a soma dos inteiros e armazena na variável "soma"
     soma = num_1 + num_2;
     
-    // imprime o valor de "soma"
     printf("O resultado da soma é %d\n", soma);
 
     return 0;
