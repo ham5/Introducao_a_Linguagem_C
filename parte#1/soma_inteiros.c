cbfe1ea22f7e7f5b5b9417d36d4fd14079c95ca5
Heitor Assis <ham5@cin.ufpe.br>
2023-02-20 22:48:34 -0300

    Update soma_inteiros.c

--- parte#1/soma_inteiros.c
+++ parte#1/soma_inteiros.c
@@ -5,16 +5,19 @@ int main(){
     int num_1;  // variável que armazenará o primeiro inteiro 
     int num_2;  // variável que armazenará o segundo inteiro
     int soma;   // variável que armazenará a soma
-
-    printf("Digite o valor do primeiro inteiro:\n");
-    scanf("%d", &num_1);    // solicita o valor do primeiro inteiro e armazena em num_1
     
+    // solicita o valor do primeiro inteiro e armazena em num_1
+    printf("Digite o valor do primeiro inteiro:\n");
+    scanf("%d", &num_1);
+    // solicita o valor do segundo inteiro e armazena em num_2
     printf("Digite o valor do segundo inteiro:\n");
-    scanf("%d", &num_2);    // solicita o valor do segundo inteiro e armazena em num_2
-
-    soma = num_1 + num_2;   // realiza a soma dos inteiros e armazena na variável "soma"
-
-    printf("O resultado da soma é %d\n", soma);   // imprime o valor de "soma"
+    scanf("%d", &num_2);
+    
+    // realiza a soma dos inteiros e armazena na variável "soma"
+    soma = num_1 + num_2;
+    
+    // imprime o valor de "soma"
+    printf("O resultado da soma é %d\n", soma);
 
     return 0;
 }
