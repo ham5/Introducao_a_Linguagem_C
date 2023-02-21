eaecc11797371c5567ae733647068d7c4cff16f2
Heitor Assis <ham5@cin.ufpe.br>
2023-02-20 22:53:13 -0300

    Update if.c

--- parte#2/if.c
+++ parte#2/if.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 
 int main(){
-    int num_1, num_2;
+    int num_1; 
+    int num_2;
 
-    printf("Informe dois inteiros abaixo:\n");
-    scanf("%d %d", &num_1, &num_2);
+    printf("Informe o valor do primeiro inteiro:\n");
+    scanf("%d", &num_1);
+    
+    printf("Informe o valor do segundo inteiro:\n");
+    scanf("%d", &num_2);
 
     // se esta condição for verdadeira, a instrução que está dentro será executada
     if (num_1 == num_2)
